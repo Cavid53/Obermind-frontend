c99a6e7193aa8dbcf9623db052c515a84429bc67
Cavid Bashirov <bashirovcavid93gmail.com>
2022-04-09 18:42:26 +0400

    Added login options

--- src/components/account/Login.js
+++ src/components/account/Login.js
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../../assets/styles/login.scss'
 
 const Login = () => {
+
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+
+    const history = useNavigate();
+
+    useEffect(()=>{
+        if(localStorage.getItem('user-info')){
+            history.push('/add')
+        }
+    },[]);
+
     return (
         <div className="container">
             <div className="row justify-content-center">
                 <form className='login-form'>
                     <div className="form-outline mb-4 mt-3">
-                        <input type="email" id="form2Example1" className="form-control" placeholder='Email address' />
+                        <input type="email" className="form-control" placeholder='Email address' onChange={(e)=>setEmail(e.target.value)} />
                     </div>
                     <div className="form-outline mb-4">
-                        <input type="password" id="form2Example2" className="form-control" placeholder='Password' />
+                        <input type="password" className="form-control" placeholder='Password' onChange={(e)=>setPassword(e.target.value)} />
                     </div>
                     <button type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
                 </form>
